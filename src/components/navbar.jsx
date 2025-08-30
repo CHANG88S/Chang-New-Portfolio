efcd45cbc10ccf7ca36a1919ade9ad90dc46773a
CHANG88S <140467098+CHANG88S@users.noreply.github.com>
2025-08-29 18:06:32 -0700

    separated Navbar
    made it a component for home.jsx to look nicer

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+export const Navbar = () => {
+    return (
+<header class="fixed top-0 w-full p-2 px-5 z-50  backdrop-blur-sm mx-[-130px] xl:mx-[-30px] xs:-mx-2" style="will-change: auto; transform: none;"> </header>
+        <nav className="flex justify-between items-center"></nav>
+            <li class="relative z-10 block cursor-pointer text-xs uppercase">
+                <a class="px-3 py-1.5 block " href="#home">home</a>
+            </li>
+            <li class="relative z-10 block cursor-pointer text-xs uppercase">
+                <a class="px-3 py-1.5 block " href="#home">home</a>
+            </li>
+            <li class="relative z-10 block cursor-pointer text-xs uppercase">
+                <a class="px-3 py-1.5 block " href="#home">home</a>
+            </li>
+            <li class="relative z-10 block cursor-pointer text-xs uppercase">
+                <a class="px-3 py-1.5 block " href="#home">home</a>
+            </li>
+            <li class="relative z-10 block cursor-pointer text-xs uppercase">
+                <a class="px-3 py-1.5 block " href="#home">home</a>
+            </li>
+        );
+    };
